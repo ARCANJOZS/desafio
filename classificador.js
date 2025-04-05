1d6f9c489c0f67968af9b5183cb2a61750345bbc
ARCANJOZS <pablohenriquearaujolima@gmail.com>
2025-04-05 21:07:56 +0000

    Atualizando classificador.js

--- classificador.js
+++ classificador.js
@@ -3,26 +3,26 @@ var nickName = "Goku";
 let xp = 100;
 
 if (xp < 1000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **FERRO**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **FERRO**");
 }
 else if (xp >=1001 && xp < 2000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **BRONZE**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **BRONZE**");
 }
 else if (xp >=2001 && xp < 5000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **PRATA**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **PRATA**");
 }
 else if (xp >=5001 && xp < 7000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **OURO**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **OURO**");
 }
 else if (xp >=7001 && xp < 8000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **PLATINA**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **PLATINA**");
 }
 else if (xp >=8001 && xp < 9000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **ASCENDENTE**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **ASCENDENTE**");
 }
 else if (xp >=9001 && xp < 10000) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **IMORTAL**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **IMORTAL**");
 }
 if (xp >=10001) {
-    console.log("O Herói de nome **" + nickName + "** está no nível de **RADIANTE**");
+    console.log("O Herói de nome **" + nickName + "** está no nível **RADIANTE**");
 }
